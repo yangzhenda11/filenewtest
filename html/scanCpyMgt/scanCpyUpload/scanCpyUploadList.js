1369c7dbf0e9078a91d65740fdbdccc47339dd23
杜伯约 <duby3@chinaunicom.cn>
2018-04-20 18:00:44 +0800

    合同扫描件上传查询列表

--- html/scanCpyMgt/scanCpyUpload/scanCpyUploadList.js
+++ html/scanCpyMgt/scanCpyUpload/scanCpyUploadList.js
@@ -1,6 +1,6 @@
 //系统的全局变量
-/*var serverPath = config.serverPath;
-var config = top.globalConfig;*/
+var config = parent.globalConfig;
+var serverPath = config.serverPath;
 
 /*
  * 初始化表格
@@ -8,52 +8,51 @@ var config = top.globalConfig;*/
 App.initDataTables('#searchContractTable', "#submitBtn", {
 	 ajax: {
         "type": "GET",
-        "url": "null",
+        "url": serverPath+'contractUpload/contractUploadList',
         "data": function(d) {//自定义传入参数
-        	d.staffId = config.staffId;
-        	d.contractNumber = $("input[name='contractNumber']").val();
-			d.contractType = $("input[name='contractType']").val();
-			//d.undertakerId = $("input[name='undertakerId']").val();
-			d.undertakeName = $("input[name='undertakeName']").val();
-			//d.oppoPartyId = $("input[name='oppoPartyId']").val();
-			d.oppoPartyName = $("input[name='oppoPartyName']").val();
+        	//d.staffId = config.staffId;
+        	d.contractNumber = $("#contractNumber").val();
+			d.contractType = $("#contractType").val();
+			d.undertakerId = $("#undertakerId").val();
+			d.undertakeName = $("#undertakeName").val();
+			d.oppoPartyId = $("#oppoPartyId").val();
+			d.oppoPartyName = $("#oppoPartyName").val();
 			d.approve_date_begin = $("input[name='approve_date_begin']").val();
 			d.approve_date_end = $("input[name='approve_date_end']").val();
+			d.approve_date = $("#approveDate").val();
             return d;
         }
     },
     "columns": [
-        {"data": "null","title": "序号"},
+        {"data": "contractId","title": "序号"},
         {"data": "contractName","title": "合同名称"},
         {"data": "contractNumber","title": "合同编号"},
-        {"data": "executeDeptCode","title": "承办部门"},
-        //{"data": "executeDeptName","title": "承办部门"},
-        //{"data": "undertakerId","title": "承办人"},
+        {"data": "executeDeptName","title": "承办部门"},
         {"data": "undertakeName","title": "承办人"},
-        //{"data": "unicomPartyId","title": "我方主体"},
-        {"data": "unicomPartyName","title": "我方主体"},
-        //{"data": "oppoPartyId","title": "对方主体"},
-        {"data": "oppoPartyName","title": "对方主体"},
-        {"data": "approve_date","title": "审批日期"},
+        {"data": "unicomPartyId","title": "我方主体"},
+        {"data": "unicomPartyName","bVisible":false,"title": "我方主体"},
+        {"data": "oppoPartyId","title": "对方主体"},
+        {"data": "oppoPartyName","bVisible":false,"title": "对方主体"},
+        {"data": "approveDate","title": "审批通过日期"},
         {
-            "data": "null",
-            "className": "text-center",
-            "title": "操作",
-            "render": function(data, type, full, meta) {
-            	if(data) {
+			"data": null,
+			"className": "text-center",
+			"title": "操作",
+			"render": function(data, type, full, meta) {
+				if(data) {
 					var btnArray = new Array();
-                    btnArray.push({ "name": "添加", "fn": "addScanCpy(\'" + data + "\')" });
-                    return App.getDataTableBtn(btnArray);;
+                    btnArray.push({ "name": "添加", "fn": "jumpContractUploadEdit()","icon":"iconfont icon-add"});
+                    return App.getDataTableBtn(btnArray);
 				} else {
 					return '';
 				}
-            }
-        }
+			}
+		},
     ]
 });
 
 //跳转到上传页面
-function addScanCpy(){
+function jumpContractUploadEdit(){
 	var src = "html/scanCpyMgt/scanCpyUpload/scanCpyUploadEdit.html?type=2&id=1";
 	App.changePresentUrl(src);
 }

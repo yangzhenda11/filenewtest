bf9d0fc57ca6b223bb73894531b95490c965bf77
zander <yangzhenda11@163.com>
2018-04-24 09:57:49 +0800

    update

--- html/scanCpyMgt/scanCpyUpload/scanCpyUploadEdit.js
+++ html/scanCpyMgt/scanCpyUpload/scanCpyUploadEdit.js
@@ -8,35 +8,37 @@ var serverPath = config.serverPath;
 //页面初始化事件
 $(function() {
 	//固定操作按钮在70px的高度
-	App.fixToolBars("toolbarBtnContent",70);
+	App.fixToolBars("toolbarBtnContent", 70);
 })
 //返回上一页
-function backPage(){
+function backPage() {
 	window.history.go(-1);
 }
 
 //初始化表格
-App.initDataTables('#scanCpyloadTable',{
+App.initDataTables('#scanCpyloadTable', {
 	ajax: {
-        "type": "POST",
-        "url": serverPath + 'listFile',
-        "data": function(d) {
-            //d.contractId = $("#contractId").val();
-            d.contractId = 110;
-            return d;
-        }
-    },
-    "columns": [
-    	{"data": "displayName","title": "文件列表"},
-    	{
-            "data": "",
-            "className": "text-center",
-            "title": "操作",
-            "render": function(data, type, full, meta) {
-            	if(data) {
-					
+		"type": "POST",
+		"url": serverPath + 'listFile',
+		"data": function(d) {
+			//d.contractId = $("#contractId").val();
+			d.contractId = 110;
+			return d;
+		}
+	},
+	"columns": [{
+			"data": "displayName",
+			"title": "文件列表"
+		},
+		{
+			"data": "",
+			"className": "text-center",
+			"title": "操作",
+			"render": function(data, type, full, meta) {
+				if(data) {
+
 				}
-            }
-        }
-    ]
+			}
+		}
+	]
 });

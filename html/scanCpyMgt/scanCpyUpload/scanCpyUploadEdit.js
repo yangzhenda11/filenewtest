2d6423e5554051adc80b9786d9b53233df46985c
zander <yangzhenda11@163.com>
2018-04-25 09:17:39 +0800

    update

--- html/scanCpyMgt/scanCpyUpload/scanCpyUploadEdit.js
+++ html/scanCpyMgt/scanCpyUpload/scanCpyUploadEdit.js
@@ -13,7 +13,6 @@ $(function() {
 		$(".portlet").css("cssText","border:none !important;padding:0px");
 		$(".page-content").removeClass("hidden");
 	}else{
-		$(".toolbarBtn,.portlet-title").removeClass("hidden");
 		$(".page-content").removeClass("hidden");
 		//固定操作按钮在70px的高度
 		App.fixToolBars("toolbarBtnContent", 70);

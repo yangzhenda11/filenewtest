508f7d7646010bf74c0f0755cca4de66b99e7559
huoliang <sw_source@163.com>
2018-04-27 16:01:02 +0800

    合同扫描件验证前台修改2

--- html/scanCpyMgt/scanValidation/scanValidation.js
+++ html/scanCpyMgt/scanValidation/scanValidation.js
@@ -28,63 +28,126 @@ $("#searchOtherSubject").click(function(){
 /*
  * 初始化表格
  */
-App.initDataTables('#getScanValidationList', "#submitBtn", {
-    ajax: {
-        "type": "POST",
-        "url": serverPath + 'sysScanValidation/listSysScanValidationInfo',
-        "data": function(d) {
-            var body = {};
-            body.start = d.start;//开始的
-            body.length = d.length;//要取的数据的
-            var formData = $("#scanValidationForm").serializeArray();//把form里面的数据序列化成数组
-            formData.forEach(function (e) {
-                d[e.name] = e.value.trim();
-            });
-            if(d.verifyStatus == 0 || d.verifyStatus == undefined){
-            	d.verifyStatus = "";
-            };
-            console.log(d);
-            return d;
-        }
-    },
-    "columns": [{
-        "data": "contractNumber",
-        "className": "text-center",
-        "title": "序号",
-        "render": function(data, type, full, meta) {
-            return meta.row + 1;
-        }
-        },
-        {"data": null,"title": "合同编号",
-        	render: function(data, type, full, meta) {
-        		console.log(data);
-				return '<a href=\"javascript:void(0)\" onclick = "jumpScanValidationView(\'' + data.verifyId + '\')">' + data.contractNumber + '</a>';
-			}
-        },
-        {"data": "contractName","title": "合同名称"},
-        {"data": "contractType","title": "合同类型"},
-        {"data": "ourPartyName","title": "我方主体"},
-        {"data": "otherPartyName","title": "对方主体"},
-        {"data": "executeDeptName","title": "承办部门"},
-        {"data": "undertakeName","title": "承办人"},
-        {"data": "verifyStatus","title": "合同验证状态",
-            "render": function(data, type, full, meta) {
-            if (data == 1) {
-                return '草稿';
-            } else if(data == 2){
-                return '审批中';
-            }else if(data == 3){
-                return '生效';
-            }else if(data == 4){
-                return '失效';
-            }else{
-                return "";
+verifyProcessTrue();
+function verifyProcessTrue() {
+    App.initDataTables('#getScanValidationList', "#submitBtn", {
+        "destroy": true,
+        ajax: {
+            "type": "POST",
+            "url": serverPath + 'sysScanValidation/listSysScanValidationInfo',
+            "data": function(d) {
+                var body = {};
+                body.start = d.start;//开始的
+                body.length = d.length;//要取的数据的
+                var formData = $("#scanValidationForm").serializeArray();//把form里面的数据序列化成数组
+                formData.forEach(function (e) {
+                    d[e.name] = e.value.trim();
+                });
+                if(d.verifyStatus == 0 || d.verifyStatus == undefined){
+                    d.verifyStatus = "";
+                };
+                return d;
             }
-        }},
-        {"data": "verifyVersion","title": "版本号"},
-        {"data": "ctreatedDateString","title": "验证日期"}
-    ]
-});
+        },
+        "columns": [{
+            "data": "contractNumber",
+            "className": "text-center",
+            "title": "序号",
+            "render": function(data, type, full, meta) {
+                return meta.row + 1;
+            }
+            },
+            {"data": null,"title": "合同编号12",
+                render: function(data, type, full, meta) {
+                    return '<a href=\"javascript:void(0)\" onclick = "jumpScanValidationView(\'' + data.verifyId + '\')">' + data.contractNumber + '</a>';
+                }
+            },
+            {"data": "contractName","className": "text-center","title": "合同名称"},
+            {"data": "typeName","className": "text-center","title": "合同类型"},
+            {"data": "ourPartyName","className": "text-center","title": "我方主体"},
+            {"data": "otherPartyName","className": "text-center","title": "对方主体"},
+            {"data": "executeDeptName","className": "text-center","title": "承办部门"},
+            {"data": "undertakeName","className": "text-center","title": "承办人"},
+            {"data": "verifyStatus","className": "text-center","title": "合同验证状态",
+                "render": function(data, type, full, meta) {
+                if (data == 1) {
+                    return '草稿';
+                } else if(data == 2){
+                    return '审批中';
+                }else if(data == 3){
+                    return '生效';
+                }else if(data == 4){
+                    return '失效';
+                }else{
+                    return "";
+                }
+            }},
+            {"data": "verifyVersion","className": "text-center","title": "版本号"},
+            {"data": "ctreatedDateString","className": "text-center","title": "验证日期"}
+        ]
+    });
+}
+
+function verifyProcessFalse() {
+    App.initDataTables('#getScanValidationList', "#submitBtn", {
+        "destroy": true,
+        ajax: {
+            "type": "POST",
+            "url": serverPath + 'sysScanValidation/listSysScanValidationInfo',
+            "data": function(d) {
+                var body = {};
+                body.start = d.start;//开始的
+                body.length = d.length;//要取的数据的
+                var formData = $("#scanValidationForm").serializeArray();//把form里面的数据序列化成数组
+                formData.forEach(function (e) {
+                    d[e.name] = e.value.trim();
+                });
+                if(d.verifyStatus == 0 || d.verifyStatus == undefined){
+                    d.verifyStatus = "";
+                };
+                return d;
+            }
+        },
+        "columns": [
+            {"className": "text-center",
+                "data": "contractId",
+                "render": function (data, type, full, meta) {
+                    return '<label class="ui-checkbox"><input id="checkchild" type="checkbox" value="' + data + '" /><span></span> </label>';
+                }
+            },
+            {"data": null,"title": "合同编号","className": "text-center",
+                render: function(data, type, full, meta) {
+                    return '<a href=\"javascript:void(0)\" onclick = "jumpScanValidationView(\'' + data.verifyId + '\')">' + data.contractNumber + '</a>';
+                }
+            },
+            {"data": "contractName","className": "text-center","title": "合同名称"},
+            {"data": "typeName","className": "text-center","title": "合同类型"},
+            {"data": "ourPartyName","className": "text-center","title": "我方主体"},
+            {"data": "otherPartyName","className": "text-center","title": "对方主体"},
+            {"data": "executeDeptName","className": "text-center","title": "承办部门"},
+            {"data": "undertakeName","className": "text-center","title": "承办人"},
+            {"data": "verifyStatus","className": "text-center","title": "合同验证状态",
+                "render": function(data, type, full, meta) {
+                    if (data == 1) {
+                        return '草稿';
+                    } else if(data == 2){
+                        return '审批中';
+                    }else if(data == 3){
+                        return '生效';
+                    }else if(data == 4){
+                        return '失效';
+                    }else{
+                        return "";
+                    }
+                }},
+            {"data": "verifyVersion","className": "text-center","title": "版本号"},
+            {"data": "ctreatedDateString","className": "text-center","title": "验证日期"}
+        ]
+    });
+}
+
+/*******************************进行分页查询数据-end******************************************* */
+
 function listScanValidationInfo(retainPaging) {
     var table = $('#getScanValidationList').DataTable();
     if(retainPaging) {
@@ -93,17 +156,19 @@ function listScanValidationInfo(retainPaging) {
         table.ajax.reload();
     }
 }
-/*******************************进行分页查询数据-end******************************************* */
+
 /*
  * 扫描件是否验证点击判断
  */
-$("input[name=optionsRadiosinline]").on("click",function(){
-	if($(this).val() == 0){
+$("input[name=verifyProcess]").on("click",function(){
+	if($(this).val() == 3){
 		$("#startValidation").removeClass("hidden");
 		$("#verifyStatus").val(0).attr("disabled","").trigger("change");
+        verifyProcessFalse();
 	}else{
 		$("#startValidation").addClass("hidden");
 		$("#verifyStatus").val(3).removeAttr("disabled").trigger("change");
+        verifyProcessTrue();
 	}
 })
 /*

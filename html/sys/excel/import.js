dd131762269b6d015caf494431a15fa80e935454
zander <yangzhenda11@163.com>
2018-06-11 15:11:14 +0800

    update

--- html/sys/excel/import.js
+++ html/sys/excel/import.js
@@ -6,3 +6,60 @@ function importContractUpload(){
     $("#importForm").submit();
     $("#importForm").attr("action",'');
 }
+
+function fileUpload(){
+	var setting = {
+		title : "文件上传导入测试",
+		isUploadFile: false,
+		url:'contractScanQuery/importContract',
+		//url:'fileload/uploadFileS3',
+		maxNumber:1,
+		//fileExtensions:['pdf']
+	};
+	function queryCallback(){
+		var fileInfo = getFileItemInfo();
+		$("#commomModal").modal("hide");
+	}
+	App.getFileUploadModal(setting,queryCallback);
+}
+function ajaxSubmit(){
+	$("#bulk").modal("show");
+}
+function gosubmit(){
+	$("#myupload").ajaxSubmit({
+        dataType: 'json',
+        url: serverPath + 'contractScanQuery/importContract',
+        resetForm: "true",
+        timeout: 3000,
+        beforeSend: function() {
+            $('#upload').html('上传中..');
+        },
+        success: function(result) {
+           	console.log(result);
+           	$('#upload').html('上传成功');
+        },
+        error: function(xhr) {
+        	console.log(result)
+        }
+    })
+}
+function ajaxSunmitfileUpload(){
+	$("#fileUpload").modal("show");
+}
+function filesubmit(){
+	$("#fileUploadssss").ajaxSubmit({
+        dataType: 'json',
+        url: serverPath + 'fileload/uploadFileS3',
+        resetForm: "true",
+        timeout: 7000,
+        beforeSend: function() {
+            $('#upload').html('上传中..');
+        },
+        success: function(result) {
+           	console.log(result)
+        },
+        error: function(xhr) {
+        	console.log(result)
+        }
+    })
+}

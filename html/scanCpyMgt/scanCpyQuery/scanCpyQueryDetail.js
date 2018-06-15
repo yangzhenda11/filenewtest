603cce7007dfb8057d65228da3454d18efaba8d3
zander <yangzhenda11@163.com>
2018-06-15 15:15:37 +0800

    修改页面中正文扫描件上传下载逻辑

--- html/scanCpyMgt/scanCpyQuery/scanCpyQueryDetail.js
+++ html/scanCpyMgt/scanCpyQuery/scanCpyQueryDetail.js
@@ -9,49 +9,67 @@ var serverPath = config.serverPath;
 $(function() {
 	//获取合同信息
 	getContractInfo();
-	
-	//获取合同正文扫描件
-    App.formAjaxJson(serverPath+'contractScanQuery/listContractText?id='+id, "POST", null, listContractTextSuccess);
-
-    function listContractTextSuccess(result) {
+	//获取合同正文和附件列表
+	getReplenishContractInfo();
+})
+/*
+ * 获取合同正文和附件列表
+ */
+function getReplenishContractInfo(){
+	var url = serverPath + "contractUpload/getReplenishContractInfo";
+	var postData = {id:id};
+	App.formAjaxJson(url, "post", JSON.stringify(postData), successCallback);
+	function successCallback(result) {
 		var data = result.data;
-		if(data.length > 0){
-			var html = "";
-			for(var i = 0; i < data.length; i++){
-				html += "<tr>"+"<td>"+ (i+1) +"</td>";
-				if(data[i].storeIdDisplay){
-					html += "<td><a href='"+serverPath+"downloadRedirect/downloadFile?storeId="+data[i].storeIdDisplay+"'>"+ data[i].displayName+"</td>";
-				}else{
-					html += "<td>"+ data[i].displayName+"</td>";
-				};
-				html += "<td>"+ App.formatDateTime(data[i].ctreatedDate,"yyyy-MM-dd") +"</td>"+
-						"<td>"+ data[i].createdByName +"</td></tr>";
-			}
-			$("#contractTextTbody").html(html);
+		if(data){
+			//设置正文扫描件html
+			setBodyDocData(data.bodyDoc);
+			//设置附件扫描件html
+			setAttachDocData(data.attachDoc);
+		}else{
+			layer.msg("暂无数据");
 		}
 	}
-	
-	//获取合同附件扫描件
-    App.formAjaxJson(serverPath+'contractScanQuery/listContractAttachment?id='+id, "POST", null, listContractAttachmentSuccess);
-
-    function listContractAttachmentSuccess(result) {
-		var data = result.data;
-		if(data.length > 0){
-			var html = "";
-			for(var i = 0; i < data.length; i++){
-				html += "<tr>"+"<td>"+ (i+1) +"</td>";
-				if(data[i].storeIdDisplay){
-					html += "<td><a href='"+serverPath+"downloadRedirect/downloadFile?storeId="+data[i].storeIdDisplay+"'>"+ data[i].displayName+"</td>";
-				}else{
-					html += "<td>"+ data[i].displayName+"</td>";
-				};
-				html += "<td>"+ App.formatDateTime(data[i].ctreatedDate,"yyyy-MM-dd") +"</td>"+
-						"<td>"+ data[i].createdByName +"</td></tr>";
-			}
-			$("#contractAttachmentTbody").html(html);
+}
+/*
+ * 设置正文扫描件html
+ */
+function setBodyDocData(bodyDocData){
+	if(bodyDocData){
+		var html = "<tr><td>1</td>";
+		if(bodyDocData.bodyDocStoreId){
+			html += "<td><a href='"+serverPath+"fileload/downloadS3?key="+bodyDocData.bodyDocStoreId+"'>"+ bodyDocData.displayName+"</td>";
+		}else{
+			html += "<td>"+ bodyDocData.displayName+"</td>";
+		};
+		html += "<td>"+ App.formatDateTime(bodyDocData.updatedDate,"yyyy-MM-dd") +"</td>"+
+				"<td>"+ (bodyDocData.updatedByName == null ? "":bodyDocData.updatedByName) +"</td></tr>";
+		
+		$("#contractTextTbody").html(html);
+	}else{
+		layer.msg("获取不到合同正文扫描件信息");
+		$("#contractTextUploadBtn").attr("disabled","disabled");
+	}
+}
+/*
+ * 设置附件扫描件html
+ */
+function setAttachDocData(data){
+	if(data.length > 0){
+		var html = "";
+		for(var i = 0; i < data.length; i++){
+			html += "<tr>"+"<td>"+ (i+1) +"</td>";
+			if(data[i].bodyDocStoreId){
+				html += "<td><a href='"+serverPath+"fileload/downloadS3?key="+data[i].bodyDocStoreId+"'>"+ data[i].displayName+"</td>";
+			}else{
+				html += "<td>"+ data[i].displayName+"</td>";
+			};
+			html += "<td>"+ App.formatDateTime(data[i].updatedDate,"yyyy-MM-dd") +"</td>"+
+					"<td>"+ (data[i].updatedByName == null ? "":data[i].updatedByName) +"</td></tr>";
 		}
-	};
-})
+		$("#contractAttachmentTbody").html(html);
+	}
+}
 /*
  * 获取合同信息
  */

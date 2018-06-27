68b9d5898a9e2238b3a5b567c4e64e6b0a4b71c7
zander <yangzhenda11@163.com>
2018-06-27 10:59:13 +0800

    工单增加撤回功能

--- fis-conf.js
+++ fis-conf.js
@@ -1,22 +1,11 @@
-// 加 md5
-fis.match('*.{js,css,png}', {
-  useHash: true
-});
-
 //命令
 //fis3 release -d ../output
 
-// 启用 fis-spriter-csssprites 插件
-//fis.match('::package', {
- // spriter: fis.plugin('csssprites')
-//});
+fis.match('*.{js,css,png}', {
+	// 加 md5
+  useHash: true
+});
 
-
-// 对 CSS 进行图片合并
-//fis.match('*.css', {
-  // 给匹配到的文件分配属性 `useSprite`
-  //useSprite: true
-//});
 
 fis.match('*.js', {
   // fis-optimizer-uglify-js 插件进行压缩，已内置
@@ -28,12 +17,3 @@ fis.match('*.css', {
   optimizer: fis.plugin('clean-css')
 });
 
-fis.match('*.png', {
-  // fis-optimizer-png-compressor 插件进行压缩，已内置
-  optimizer: fis.plugin('png-compressor')
-});
-fis.media('debug').match('*.{js,css,png}', {
-  useHash: false,
-  //useSprite: false,
-  optimizer: null
-})

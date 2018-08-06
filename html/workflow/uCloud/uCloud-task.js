c15e63b76e9bb29f90fef7cde13d993e744dd545
zander <yangzhenda11@163.com>
2018-08-06 19:43:36 +0800

    更改关闭页面

--- html/workflow/uCloud/uCloud-task.js
+++ html/workflow/uCloud/uCloud-task.js
@@ -96,7 +96,7 @@ function jumpSanCpyQueryDetail(businessId,taskDefinitionKey,processInstanceId){
 	}
 }
 function closeWindow(){
-    if (navigator.userAgent.indexOf("Firefox") != -1 || navigator.userAgent.indexOf("Chrome") !=-1) {
+    if (navigator.userAgent.indexOf("Firefox") != -1 || navigator.userAgent.indexOf("Chrome") != -1) {
         window.location.href="about:blank";
         window.close();
     } else {

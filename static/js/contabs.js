533093a10cc831e92be4e242018faeefd8c42c81
zander <yangzhenda11@163.com>
2018-08-06 21:07:46 +0800

    update

--- static/js/contabs.js
+++ static/js/contabs.js
@@ -151,10 +151,11 @@ function conTabC() {
 					if($(this).data("id") == o) {
 						$(this).show().siblings(".J_iframe").hide();
 						var dataTablesDom = $(this).contents().find(".dataTables_scrollHeadInner");
-						if(dataTablesDom.css("width") == "100px"){
+						var dataTablesDomParent = dataTablesDom.parent();
+						if(dataTablesDom.css("width") != dataTablesDomParent.css("width")){
 							var parentName = $(this)[0].id;
 							var tableId = dataTablesDom.parents(".dataTables_wrapper")[0].id.split("_")[0];
-$("#"+parentName+"")[0].contentWindow.$('#'+tableId+'').DataTable().draw();
+							$("#"+parentName+"")[0].contentWindow.$('#'+tableId+'').DataTable().draw();
 						}
 						return false
 					}
@@ -289,12 +290,14 @@ function conTabJ() {
 
 function conTabE() {
 	if(!$(this).hasClass("active")) {
+		alert(1);
 		var k = $(this).data("id");
 		$(".J_mainContent .J_iframe").each(function() {
 			if($(this).data("id") == k) {
 				$(this).show().siblings(".J_iframe").hide();
 				var dataTablesDom = $(this).contents().find(".dataTables_scrollHeadInner");
-				if(dataTablesDom.css("width") == "100px"){
+				var dataTablesDomParent = dataTablesDom.parent();
+				if(dataTablesDom.css("width") != dataTablesDomParent.css("width")){
 					var parentName = $(this)[0].id;
 					var tableId = dataTablesDom.parents(".dataTables_wrapper")[0].id.split("_")[0];
 $("#"+parentName+"")[0].contentWindow.$('#'+tableId+'').DataTable().draw();

31aeb2aedc9a9e88dae48485c36c2b9ed2a53d29
zander <yangzhenda11@163.com>
2018-09-28 13:58:24 +0800

    增加datatable成功时的回调函数

--- static/js/app.js
+++ static/js/app.js
@@ -1014,6 +1014,10 @@ var App = function() {
 				}else if(xhr.status == 200){
 		        	if(xhr.responseJSON.status != 1){
 		        		layer.alert(xhr.responseJSON.message,{icon:2,title:"错误"});
+		        	}else{
+		        		if(successCallback){
+			        		successCallback(xhr.responseJSON);
+			        	};
 		        	}
 		       	}else if(xhr.status == 401){
 		       		if(top.globalConfig.loginSwitchSuccess == 0){

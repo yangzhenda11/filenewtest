de171f314bc6aca277a9ca2892f860aa08d60d02
zander <yangzhenda11@163.com>
2018-10-29 17:41:44 +0800

    注释fis3关于js文件的注释

--- fis-conf.js
+++ fis-conf.js
@@ -28,10 +28,10 @@ fis.match('*.{js,css,jpg,png}', {
   	query: '?t=' + fis.get('new date')
 });
 
-fis.match('*.js', {
-	// fis-optimizer-uglify-js 插件进行压缩，已内置
-	optimizer: fis.plugin('uglify-js')
-});
+//fis.match('*.js', {
+//	// fis-optimizer-uglify-js 插件进行压缩，已内置
+//	optimizer: fis.plugin('uglify-js')
+//});
 //
 //fis.match('*.css', {
 //	// fis-optimizer-clean-css 插件进行压缩，已内置

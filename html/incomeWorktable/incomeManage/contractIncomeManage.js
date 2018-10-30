efe3fdd727cfe578e43f01c289cfa05ed07d2d55
shihui <shihui0531@163.com>
2018-10-30 15:15:23 +0800

    收入预测跳转线路收入预测方法参数名称调整

--- html/incomeWorktable/incomeManage/contractIncomeManage.js
+++ html/incomeWorktable/incomeManage/contractIncomeManage.js
@@ -85,6 +85,6 @@ function initContractInforTable(){
  */
 function jumpLineManage(contractNumber){
 	var url = "/html/incomeWorktable/incomeManage/lineIncomeManage.html?contractNumber="
-		+contractNumber+"&accountPeriodName="+parm.forecastAccountPeriod+"&returnbtn=true";
+		+contractNumber+"&accountPeriodName="+parm.accountPeriodName+"&returnbtn=true";
 	App.changePresentUrl(url);
 }

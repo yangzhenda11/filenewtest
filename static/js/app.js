f64a84ad9f7788a6440aaf6f058061632e1cad06
zander <yangzhenda11@163.com>
2018-11-10 16:06:06 +0800

    修好超时提示

--- static/js/app.js
+++ static/js/app.js
@@ -675,7 +675,7 @@ var App = function() {
 			        		});
 						}
 	        		}else{
-	        			layer.alert("接口错误", {icon: 2,title:"错误"});
+	        			layer.alert("接口请求超时", {icon: 2,title:"错误"});
 	        		};
 				}
 			});
@@ -695,7 +695,7 @@ var App = function() {
 	        		});
 				}
     		}else{
-    			layer.alert("接口错误", {icon: 2,title:"错误"});
+    			layer.alert("接口请求超时", {icon: 2,title:"错误"});
     		};
 		},
 		//datatable中button点击或者提交后台时显示提交中的禁用选项(设置：data-loading-text)
@@ -1031,7 +1031,7 @@ var App = function() {
 		        		});
 					}
 		        }else{
-        			layer.alert("接口错误", {icon: 2,title:"错误"});
+        			layer.alert("接口请求超时", {icon: 2,title:"错误"});
         		}
 		    });
 			$.fn.dataTable.ext.errMode = 'throw';
@@ -1931,7 +1931,7 @@ function onAsyncError(event, treeId, treeNode, XMLHttpRequest, textStatus, error
     		});
 		}
 	}else{
-		layer.alert("接口错误", {icon: 2,title:"错误"});
+		layer.alert("接口请求超时", {icon: 2,title:"错误"});
 	};
 }
 /*

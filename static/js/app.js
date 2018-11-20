6a094adede5daa42974c83faa07528375d62205e
zander <yangzhenda11@163.com>
2018-11-20 11:25:25 +0800

    update

--- static/js/app.js
+++ static/js/app.js
@@ -1996,6 +1996,27 @@ String.prototype.trim = function() {
     return this.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
 }
 /*
+ * indexOf兼容IE8
+ */
+if (!Array.prototype.indexOf){
+	Array.prototype.indexOf = function(elt /*, from*/){
+    var len = this.length >>> 0;
+    var from = Number(arguments[1]) || 0;
+    from = (from < 0)
+         ? Math.ceil(from)
+         : Math.floor(from);
+    if (from < 0)
+      from += len;
+    for (; from < len; from++)
+    {
+      if (from in this &&
+          this[from] === elt)
+        return from;
+    }
+    return -1;
+  	};
+}
+/*
  * datatable跳转至第**页
  */
 function setDatatableCurPage(dom){

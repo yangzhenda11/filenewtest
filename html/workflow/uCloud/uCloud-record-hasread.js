b5848d4f03d2802d099130e7bd3946c7ee551bc7
weifeng <weifengjava@sina.com>
2018-11-29 19:07:20 +0800

    云门户待阅

--- html/workflow/uCloud/uCloud-record-hasread.js
+++ html/workflow/uCloud/uCloud-record-hasread.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 var config = top.globalConfig;
 var serverPath = config.serverPath;
-=======
-//var config = top.globalConfig;
-//var serverPath = config.serverPath;
->>>>>>> eb44b9b1453ddb6026370bffe6e4b1ee2c62ec2f
 //当前页面参数获取，针对不同的参数处理代办跳转还是数据列表跳转的页面差异项
 var parm = App.getPresentParm();
 var staffOrgId=parm.staffOrgId;//岗位ID接收人

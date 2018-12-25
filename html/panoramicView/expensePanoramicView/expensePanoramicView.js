351b1f8a2138757fdcab4378f7e918a6c55b538c
zander <yangzhenda11@163.com>
2018-12-25 14:01:37 +0800

    地铁图本地提交

--- html/panoramicView/expensePanoramicView/expensePanoramicView.js
+++ html/panoramicView/expensePanoramicView/expensePanoramicView.js
@@ -293,16 +293,44 @@ function initExpenseFlowCharts(data){
 	})
 }
 $("#expenseFlowChart").on('mouseenter',"circle",function(e){
+	console.log(e)
     if($(this).data("status") == 2){
-    	var html = "<input type='checkbox' disabled='disabled' ><span style='color:#000'>测试</span>"
-		layer.tips(html, $(this), {
-		  tips: [1, '#fff'],
-		  time: 0
-		});
+    	var addHtml = "<input type='checkbox' disabled='disabled' ><span style='color:#000'>测试</span>"
+    	var html = '<div id="vtip"><img id="vtipArrow" src="/static/img/vtip_arrow.png" />' + addHtml + '</div>';
+    	this.top = (e.pageY + 20); this.left = (e.pageX - 0);
+        $('body').append(html);
+        $('div#vtip').css("top", this.top+"px").css("left", this.left+"px").fadeIn("slow");
     }
  });
 $("#expenseFlowChart").on('mouseout',"circle",function(e){
-    layer.closeAll('tips');
+   $("div#vtip").fadeOut("slow").remove();
 });
+//
+//this.vtip = function() {    
+//  this.xOffset = -10; // x distance from mouse
+//  this.yOffset = 10; // y distance from mouse       
+//  
+//  $(".vtip").unbind().hover(    
+//      function(e) {
+//          this.t = this.title;
+//          this.title = ''; 
+//          
+//          
+//      },
+//      function() {
+//          this.title = this.t;
+//          //$("div#vtip").fadeOut("slow").remove();
+//      }
+//  ).mousemove(
+//      function(e) {
+//          this.top = (e.pageY + yOffset);
+//          this.left = (e.pageX + xOffset);
+//                       
+//          $("div#vtip").css("top", this.top+"px").css("left", this.left+"px");
+//      }
+//  );            
+//  
+//};
+
 
 /**************************************地铁图配置********************************************/
